0e4b5dfe42cb5db5444e450a9dc1dec51d3edb79
D Pyrkova <dpyrkova@unitarius.ru>
2023-07-08 20:39:37 +0300

    L05 modul test stack

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,7 +2,7 @@ from hexlet_pytest.example import reverse
 
 
 def test_reverse():
-    assert reverse('Hexlet') == 'telxeh'
+    assert reverse('Hexlet') == 'telxeH'
 
 
 def test_reverse_for_empty_string():
